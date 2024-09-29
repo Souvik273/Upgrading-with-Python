49b4dc40e7a7284e07f7f5e82fcd3e002c1d46f8
Souvik273 <souvikgoswami464@gmail.com>
2024-09-29 12:45:27 +0530

    Python basics

---
+++ Basics1.py
@@ -0,0 +1,41 @@
+# # Data Types in python
+# # Integer
+# a = 5
+# print("Type of a: ", type(a))
+#
+# b = 5.0
+# print("\nType of b: ", type(b))
+#
+# c = 2 + 4j
+# print("\nType of c: ", type(c))
+#
+# #A string is a collection of one or more characters put in a single quote, double-quote or triple quote. In python there is no character data type, a character is a string of length one. It is represented by str class.
+#
+# # Creating a String
+# # with single Quotes
+# String1 = 'Welcome to the Geeks World'
+# print("String with the use of Single Quotes: ")
+# print(String1)
+#
+# # Creating a String
+# # with double Quotes
+# String1 = "I'm a Geek"
+# print("\nString with the use of Double Quotes: ")
+# print(String1)
+# print(type(String1))
+#
+# # Creating a String
+# # with triple Quotes
+# String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+# print("\nString with the use of Triple Quotes: ")
+# print(String1)
+# print(type(String1))
+#
+# # Creating String with triple
+# # Quotes allows multiple lines
+# String1 = '''Geeks
+#             For
+#             Life'''
+# print("\nCreating a multiline String: ")
+# print(String1)
+
