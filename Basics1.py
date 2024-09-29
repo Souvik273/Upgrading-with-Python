6fae30711e8ae19705b805a3eebda63e2aee30fe
Souvik273 <souvikgoswami464@gmail.com>
2024-09-29 13:43:01 +0530

    Data Types is python ( Integer , String , Boolean ,Set , Tuple , Dictionary )

--- Basics1.py
+++ Basics1.py
@@ -1,43 +1,45 @@
-# # Data Types in python
-# # Integer
-# a = 5
-# print("Type of a: ", type(a))
-#
-# b = 5.0
-# print("\nType of b: ", type(b))
-#
-# c = 2 + 4j
-# print("\nType of c: ", type(c))
-#
+# # --------------------------------Data Types in python ---------------------------------------
+# # ---------------------------------------Integer-----------------------------------------------
+a = 5
+print("Type of a: ", type(a))
+
+b = 5.0
+print("\nType of b: ", type(b))
+
+c = 2 + 4j
+print("\nType of c: ", type(c))
+
+#-----------------------------------------String-------------------------------------------------------
+
 # #A string is a collection of one or more characters put in a single quote, double-quote or triple quote. In python there is no character data type, a character is a string of length one. It is represented by str class.
 #
 # # Creating a String
-# # with single Quotes
-# String1 = 'Welcome to the Geeks World'
-# print("String with the use of Single Quotes: ")
-# print(String1)
+# with single Quotes
+String1 = 'Welcome to the Geeks World'
+print("String with the use of Single Quotes: ")
+print(String1)
 #
 # # Creating a String
 # # with double Quotes
-# String1 = "I'm a Geek"
-# print("\nString with the use of Double Quotes: ")
-# print(String1)
-# print(type(String1))
+String1 = "I'm a Geek"
+print("\nString with the use of Double Quotes: ")
+print(String1)
+print(type(String1))
 #
 # # Creating a String
 # # with triple Quotes
-# String1 = '''I'm a Geek and I live in a world of "Geeks"'''
-# print("\nString with the use of Triple Quotes: ")
-# print(String1)
-# print(type(String1))
+String1 = '''I'm a Geek and I live in a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+print(type(String1))
 #
 # # Creating String with triple
 # # Quotes allows multiple lines
-# String1 = '''Geeks
-#             For
-#             Life'''
-# print("\nCreating a multiline String: ")
-# print(String1)
+String1 = '''Geeks
+            For
+            Life'''
+print("\nCreating a multiline String: ")
+print(String1)
 
 #Accessing elements of String
 
@@ -54,4 +56,241 @@ print("\nLast character of String is: ")
 print(String1[-1])
 print(String1[-5])
 
-print('I am just pushing')
+# ---------------------------------------------LIst-----------------------------------------------------
+# Creating a list
+#Lists are just like arrays, declared in other languages which is an ordered collection of data.
+#Creating a list : -
+List = []
+print("Initial blank List: ")
+print(List)
+
+# Creating a List with
+# the use of a String
+List = ['GeeksForGeeks']
+print("\nList with the use of String: ")
+print(List)
+
+# Creating a List with
+# the use of multiple values
+List = ["Geeks", "For", "Geeks"]
+print("\nList containing multiple values: ")
+print(List[0])
+print(List[2])
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List = [['Geeks', 'For'], ['Geeks']]
+print("\nMulti-Dimensional List: ")
+print(List)
+
+# Python program to demonstrate
+# accessing of element from list
+
+# Creating a List with
+# the use of multiple values
+List = ["Geeks", "For", "Geeks"]
+
+# accessing a element from the
+# list using index number
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+# accessing a element using
+# negative indexing
+print("Accessing element using negative indexing")
+
+# print the last element of list
+print(List[-1])
+
+# print the third last element of list
+print(List[-3])
+
+
+#-------------------------------------------Tuple--------------------------------------------------
+
+# Just like a list, tuple is also an ordered collection of Python objects.
+# tuples are immutable i.e. tuples cannot be modified after it is created. It is represented by tuple class.
+#  tuples are created by placing a sequence of values separated by ‘comma’ with or without the use of parentheses
+# Note: Tuples can also be created with a single element, but it is a bit tricky. Having one element in the parentheses is not sufficient, there must be a trailing ‘comma’ to make it a tuple.
+
+# Python program to demonstrate
+# creation of Set
+
+# Creating an empty tuple
+Tuple1 = ()
+print("Initial empty Tuple: ")
+print(Tuple1)
+
+# Creating a Tuple with
+# the use of Strings
+Tuple1 = ('Geeks', 'For')
+print("\nTuple with the use of String: ")
+print(Tuple1)
+
+# Creating a Tuple with
+# the use of list
+list1 = [1, 2, 4, 5, 6]
+print("\nTuple using List: ")
+print(tuple(list1))
+
+# Creating a Tuple with the
+# use of built-in function
+Tuple1 = tuple('Geeks')
+print("\nTuple with the use of function: ")
+print(Tuple1)
+
+# Creating a Tuple
+# with nested tuples
+Tuple1 = (0, 1, 2, 3)
+Tuple2 = ('python', 'geek')
+Tuple3 = (Tuple1, Tuple2)
+print("\nTuple with nested tuples: ")
+print(Tuple3)
+
+# Python program to
+# demonstrate accessing tuple
+
+tuple1 = tuple([1, 2, 3, 4, 5])
+
+# Accessing element using indexing
+print("First element of tuple")
+print(tuple1[0])
+
+# Accessing element from last
+# negative indexing
+print("\nLast element of tuple")
+print(tuple1[-1])
+
+print("\nThird last element of tuple")
+print(tuple1[-3])
+
+
+#--------------------------------------Boolean---------------------------------------------
+
+# Data type with one of the two built-in values, True or False.
+# Python program to
+# demonstrate boolean type
+
+
+print(type(True))
+print(type(False))
+
+# print(type(true))                   # NameError: name 'true' is not defined
+
+
+#-------------------------------------------------Set-----------------------------------------------
+
+# In Python, Set is an unordered collection of data types that is iterable, mutable, and has no duplicate elements.
+
+# Sets can be created by using the built-in set() function with an iterable object or a sequence by placing the sequence inside curly braces, separated by a ‘comma’.
+
+# Python program to demonstrate
+# Creation of Set in Python
+
+# Creating a Set
+set1 = set()
+print("Initial blank Set: ")
+print(set1)
+
+# Creating a Set with
+# the use of a String
+set1 = set("GeeksForGeeks")
+print("\nSet with the use of String: ")
+print(set1)
+
+# Creating a Set with
+# the use of a List
+set1 = set(["Geeks", "For", "Geeks"])
+print("\nSet with the use of List: ")
+print(set1)
+
+# Creating a Set with
+# a mixed type of values
+# (Having numbers and strings)
+set1 = set([1, 2, 'Geeks', 4, 'For', 6, 'Geeks'])
+print("\nSet with the use of Mixed Values")
+print(set1)
+
+
+# Accessing elements of Sets
+# Set items cannot be accessed by referring to an index, since sets are unordered the items has no index. But you can loop through the set items using a for loop, or ask if a specified value is present in a set, by using in keyword.
+
+# Python program to demonstrate
+# Accessing of elements in a set
+
+# Creating a set
+set1 = set(["Geeks", "For", "Geeks"])
+print("\nInitial set")
+print(set1)
+
+# Accessing element using
+# for loop
+print("\nElements of set: ")
+for i in set1:
+    print(i, end=" ")
+
+# Checking the element
+# using in keyword
+print("Geeks" in set1)
+
+
+#----------------------------------------Dictionary-----------------------------------------
+
+# Dictionary in Python is an unordered collection of data values,
+# Dictionary holds key:value pair
+#  Each key-value pair in a Dictionary is separated by a colon :, whereas each key is separated by a ‘comma’
+#Creating Dictionary
+# In Python, a Dictionary can be created by placing a sequence of elements within curly {} braces, separated by ‘comma’.
+#  keys can’t be repeated and must be immutable.
+# Dictionary can also be created by the built-in function dict().
+
+# Creating an empty Dictionary
+Dict = {}
+print("Empty Dictionary: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Integer Keys
+Dict = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
+print("\nDictionary with the use of Integer Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with Mixed keys
+Dict = {'Name': 'Geeks', 1: [1, 2, 3, 4]}
+print("\nDictionary with the use of Mixed Keys: ")
+print(Dict)
+
+# Creating a Dictionary
+# with dict() method
+Dict = dict({1: 'Geeks', 2: 'For', 3: 'Geeks'})
+print("\nDictionary with the use of dict(): ")
+print(Dict)
+
+# Creating a Dictionary
+# with each item as a Pair
+Dict = dict([(1, 'Geeks'), (2, 'For')])
+print("\nDictionary with each item as a pair: ")
+print(Dict)
+
+# Accessing elements of Dictionary
+# In order to access the items of a dictionary refer to its key name. Key can be used inside square brackets. There is also a method called get() that will also help in accessing the element from a dictionary.
+
+# Python program to demonstrate
+# accessing a element from a Dictionary
+
+# Creating a Dictionary
+Dict = {1: 'Geeks', 'name': 'For', 3: 'Geeks'}
+
+# accessing a element using key
+print("Accessing a element using key:")
+print(Dict['name'])
+
+# accessing a element using get()
+# method
+print("Accessing a element using get:")
+print(Dict.get(3))
+
+
+
